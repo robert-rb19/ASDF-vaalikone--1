bb10aa702e9f5da91e907d2675fde5cdea84d0ad
peabert <78085323+peabert@users.noreply.github.com>
2021-06-02 07:55:23 +0300

    update

--- Election_machine/src/Candidate.java
+++ Election_machine/src/Candidate.java
@@ -1,9 +1,60 @@
-
 public class Candidate {
+	protected int id;
+	protected String fullname;
+	protected String email;
+	protected String password;
+public Candidate() {
+	
+}
 
-	public String get(String fullname) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+public Candidate(int id) {
+	this.id = id;
+}
+
+public Candidate (int id, String fullname,String email,String password) {
+	this(fullname, email, password);
+	this.id = id;
+}
+
+public Candidate (String fullname,String email,String password) {
+	this.fullname = fullname;
+	this.email = email;
+	this.password = password;
+}
+
+public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public String getFullname() {
+	return fullname;
+}
+
+public void setFullname(String fullname) {
+	this.fullname = fullname;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
+
+
+
 
 }
--- Election_machine/src/answer.java
+++ Election_machine/src/answer.java
@@ -1,4 +1,25 @@
 
+
 public class answer {
+protected String answers;
+
+
+public answer() {
+	
+}
+
+public answer (String answers) {
+	this.answers = answers;
+}
+
+
+public String getanswer() {
+	return answers;
+}
+
+public void setanswer(String answers) {
+	this.answers = answers;
+}
+
 
 }
